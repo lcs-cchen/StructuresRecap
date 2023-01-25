2d74adda7eb9140fdf409ddb4cd95103c6456bb4
cyrus chen <pschen@lcs.on.ca>
2023-01-25 11:19:47 -0500

    Initial Commit

---
+++ StructuresRecap/StructuresRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructuresRecapApp.swift
+//  StructuresRecap
+//
+//  Created by Cyrus Chen on 25/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct StructuresRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
